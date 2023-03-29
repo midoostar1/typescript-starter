1cf2c5c85515d453e482a7d57e39428a39655448
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-03-29 00:00:51 -0500

    type checking in functions

---
+++ functions.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function singnUpUser(name, email, isPaid) {
+}
+function loginUser(name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+}
+addTwo(5);
+getUpper('mido');
+singnUpUser('mido', 'mido@codeup', true);
+loginUser('midoostar', 'midoostar@codeup');
--- functions.ts
+++ functions.ts
@@ -1,3 +1,26 @@
-function addTwo(){
-    
+function addTwo(num: number){
+return num + 2
 }
+
+function getUpper(val: string){
+return val.toUpperCase()
+}
+
+
+function singnUpUser(name: string, email: string, isPaid: boolean){
+
+}
+
+function loginUser(name: string, email: string, isPaid: boolean = false){
+
+}
+
+addTwo(5)
+getUpper('mido')
+singnUpUser('mido', 'mido@codeup', true)
+
+loginUser('midoostar', 'midoostar@codeup')
+
+
+
+export{}
