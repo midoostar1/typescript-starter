7be3cf4bfcc7a23ea7344fcff3c0570b59a7ae06
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-03-29 02:35:49 -0500

    tuples in typescript

---
+++ tuples.ts
@@ -0,0 +1,12 @@
+//const user: (string | number)[] = ['hc',1]
+
+let user: [string, number, boolean] 
+
+user  = ['MIDO', 12345, true, ]
+//user = [122, true, 'mido']
+let rgb: [number, number, number] = [245,432,345,]
+
+let newUser: [number, string] = [123, 'mido@email.com', ]
+
+newUser[1] = 'mido@email.mail'
+//newUser.push(true)
