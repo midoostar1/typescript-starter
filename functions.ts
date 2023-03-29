95cd367ddf55a8d96ffcee37a733274de3055a46
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-03-29 00:37:48 -0500

    type checking in function return types

--- functions.ts
+++ functions.ts
@@ -1,4 +1,4 @@
-function addTwo(num: number){
+function addTwo(num: number): number {
 return num + 2
 }
 
@@ -14,6 +14,38 @@ function singnUpUser(name: string, email: string, isPaid: boolean){
 function loginUser(name: string, email: string, isPaid: boolean = false){
 
 }
+
+
+// function getValue(myVal: number){
+//     if(myVal > 5){
+//       return true  
+//     }
+//     return '200 OK'
+// }
+
+
+const getHello = (s:string):string =>{
+return ''
+}
+
+
+const heros = ['thor', 'spiderman', 'ironman']
+
+heros.map((hero): string =>{
+    return `this is a ${hero}`
+})
+
+
+function handleError(errmsg: string): never {
+throw new Error(errmsg)
+}
+
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg)
+    }
+
+
 
 addTwo(5)
 getUpper('mido')
